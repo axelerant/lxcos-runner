0e50daa32fcb8a53ebe9da5ed8d7e9753a2b76b2
Medhamsh V <medhamsh@axelerant.com>
2014-07-31 18:30:42 +0530

    Adding pseudo blocks for logics

--- runner/examine.rb
+++ runner/examine.rb
@@ -17,3 +17,17 @@ def number_of_containers
   c.count
 end
 
+#def run_in_all_nodes
+#  Chef::Config.from_file(File.expand_path('~/.chef/knife.rb'))
+#  Chef::Knife.run(%w(ssh 'name:*' -x ubuntu -i /path/to/key "run_number_of_containers")
+#end
+
+#def provision_new_node
+#Logic to see the number of containers and provision new node if >100
+#end
+
+#def intimate_node
+#Intimate the current active node to webapp(projsapce)
+#so that ir provisions containers in that node
+#end
+
