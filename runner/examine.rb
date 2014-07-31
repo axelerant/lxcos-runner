d073204db27b5b569fd4965bb06efca32898e4b4
Medhamsh V <medhamsh@axelerant.com>
2014-07-31 18:20:03 +0530

    Adding nodelist and number of containers methods

--- runner/examine.rb
+++ runner/examine.rb
@@ -1,4 +1,14 @@
 require 'lxc'
+require 'chef'
+require 'chef/knife'
+
+#List out all the nodes
+def node_list
+  Chef::Config.from_file(File.expand_path('~/.chef/knife.rb'))
+  Chef::Node.list.each do |node|
+	puts node.first
+  end
+end
 
 #List out the number of containers irrespective to the state
 
@@ -7,4 +17,3 @@ def number_of_containers
   c.count
 end
 
-number_of_containers
