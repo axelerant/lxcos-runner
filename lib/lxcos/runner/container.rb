de97dc561696c0b2c74a5b4b25a472e91f0e0cd2
Tapan Sharma <sharma.tapan27@gmail.com>
2014-08-26 14:22:00 +0530

    Code related to cloning of container

--- lib/lxcos/runner/container.rb
+++ lib/lxcos/runner/container.rb
@@ -1,16 +1,46 @@
 module Lxcos
   module Runner
     class Container
-      attr_accessor :name, :details
+      # attr_accessor :name, :details
+      attr_accessor :name, :type, :memory, :cpus
       
-      def initialize(name)
-        @name = name
+      def initialize(name, type, memory, cpus)
+          @name = name
+          @type = type
+          @memory = memory
+          @cpus = cpus
+          container = new_container(@type)
+          container.clone(@name, 
+            flags: LXC::LXC_CLONE_SNAPSHOT, bdev_type: 'overlayfs')
       end
 
+
+      def new_container(type)
+        LXC::Container.new(type)
+      end
+
+      def create_and_start
+        @container = new_container(@name)
+        @container.start
+        sleep(5)
+      end
+
+      def set_cgroup_limits
+        @container.set_cgroup_item("memory.limit_in_bytes", @memory)
+        @container.set_cgroup_item("cpuset.cpus", @cpus)
+      end
+
+      def attach
+        @container.attach do 
+        #run custom commands inside containers
+        end 
+      end
+ 
       def create
-        @details = Node.create_container(name)
+        @details = Node.create_container(@name)
       end
 
     end
   end
 end
+
