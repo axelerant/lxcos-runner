09c800628c5079934d151eb414468485c505ac4e
Devaroop <devaroop123@yahoo.co.in>
2014-11-10 20:24:51 +0530

    passwordless ssh to container from node implement

--- lib/lxcos/runner/provision/container.rb
+++ lib/lxcos/runner/provision/container.rb
@@ -20,8 +20,16 @@ module Lxcos
         end
 
 	container_ip = JSON.parse(container_hash)["ip_addr"].first
+
 	Net::SSH.start(node_ip, 'ubuntu') do |session|
+          #haproxy cookbook
 	  session.exec!("sudo chef-client -o 'role[haproxy]'")
+
+          #passwordless ssh
+          session.exec!("mkdir -p /opt/goatos/.local/share/lxc/#{name}/delta0/home/ubuntu/.ssh")
+          session.exec!("cp /opt/goatos/.ssh/id_rsa.pub /opt/goatos/.local/share/lxc/#{name}/delta0/home/ubuntu/.ssh/authorized_keys")
+          session.exec!("chown 165536.165536 /opt/goatos/.local/share/lxc/#{name}/delta0/home");
+
         end
 
         {node_name: active_node.name,
