2d40a7260f697d547e19263c691df87103018640
Medhamsh V <medhamsh@axelerant.com>
2014-12-10 12:32:31 +0530

    fixing sharedrepository

--- config/deploy/projspace.rb
+++ config/deploy/projspace.rb
@@ -30,7 +30,7 @@ namespace :environment do
   desc 'Deploy code for the environment'
   task :deploy_code do
     on roles(:app) do
-      execute Container.execute("sudo rm /home/#{ENV['project_name']}/www/#{ENV['environment_name']} -r && git clone #{ENV['repo_url']} /home/#{ENV['project_name']}/www/#{ENV['environment_name']} -b #{ENV['repo_branch']} && git config core.sharedRepository true && ln -s /home/#{ENV['project_name']}/files/#{ENV['environment_name']} /home/#{ENV['project_name']}/www/#{ENV['environment_name']}/docroot/sites/default/files && sh /home/#{ENV['project_name']}/www/#{ENV['environment_name']}/projspace-utils/#{ENV['environment_name']}-postDeployHook")
+      execute Container.execute("sudo rm /home/#{ENV['project_name']}/www/#{ENV['environment_name']} -r && git clone #{ENV['repo_url']} /home/#{ENV['project_name']}/www/#{ENV['environment_name']} -b #{ENV['repo_branch']} && cd /home/#{ENV['project_name']}/www/#{ENV['environment_name']} && git config core.sharedRepository true && ln -s /home/#{ENV['project_name']}/files/#{ENV['environment_name']} /home/#{ENV['project_name']}/www/#{ENV['environment_name']}/docroot/sites/default/files && sh /home/#{ENV['project_name']}/www/#{ENV['environment_name']}/projspace-utils/#{ENV['environment_name']}-postDeployHook")
     end
   end
 
