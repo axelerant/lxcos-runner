c4d4f94a5edd24aa118f1a9749f3642c4f7b3824
Tapan Sharma <sharma.tapan27@gmail.com>
2015-03-17 10:41:49 +0530

    1) Add vhsot to node while creating container 2) collect www-data user keys

--- lib/lxcos/runner/provision/container.rb
+++ lib/lxcos/runner/provision/container.rb
@@ -21,6 +21,11 @@ module Lxcos
 
 	container_ip = JSON.parse(container_hash)["ip_addr"].first
 
+  Net::SSH.start(node_ip, 'ubuntu') do |session|
+    session.exec!("hooks_host.rb -n #{active_node.name} -i #{node_ip} -p #{name}")
+    session.exec!("sudo a2ensite #{name}.hooks.#{active_node}.conf && sudo /etc/init.d/apache2 reload")
+  end
+
 	Net::SSH.start(node_ip, 'ubuntu') do |session|
           #haproxy cookbook
 	  session.exec!("sudo chef-client -o 'role[haproxy]'")
@@ -29,16 +34,18 @@ module Lxcos
           session.exec!("sudo add_key_to_container #{name}")
         end
 
-        container_key = ""
+        container_key_ubuntu = "", container_key_www_data = ""
         Net::SSH.start(node_ip, 'goatos') do |session|
-          container_key = session.exec!("ssh -o LogLevel=quiet -A -t -t -o StrictHostKeyChecking=no ubuntu@#{container_ip} 'gen_keys ubuntu'")
+          keys = session.exec!("ssh -o LogLevel=quiet -A -t -t -o StrictHostKeyChecking=no ubuntu@#{container_ip} 'gen_keys ubuntu'")
+          container_key_ubuntu, container_key_www_data = keys.strip.split("\r\n")
         end
 
         {node_name: active_node.name,
           node_ip: node_ip,
           container_ip: container_ip,
           container_name: name,
-          container_key: container_key
+          container_key_ubuntu: container_key_ubuntu,
+          container_key_www_data: container_key_www_data
         }
       rescue => e
         puts e
